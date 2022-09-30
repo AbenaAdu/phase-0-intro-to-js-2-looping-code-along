2d85f7b707ebdd3292aa77b3ae5c3c94aa1d4015
Abena Adu <abeanadu@gmail.com>
2022-09-30 00:53:41 -0400

    commit tree 698bb1ccf945f287bd4b1eceac0f215b0c3000dc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown() {
+    let i = 11;
+    while(i > 0) {
+        i--;
+        console.log(i);
+    }
+}
